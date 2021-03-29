6a246a393725340f000ee240ffd67f239c1c4df7
Hannah <adrizzplaysmc@gmail.com>
2021-03-29 02:09:37 +0200

    gnu is a good license, i think

--- twitterbot.py
+++ twitterbot.py
@@ -39,7 +39,29 @@ approved = [578219178066968576, 752163420962422795]
 channels = [823651597971750922, 824056645205753897]
 guilds = [752157598232477786, 576153162394894376]
 
+@bot.command(brief="Bot license", description="Displays the GNU GPLv3 license for the bot's code, including its Github link")
+async def license(ctx):
+    await ctx.send("""This bot is licensed under the GNU VERSION 3 GENERAL PUBLIC LICENSE.
 
+    The GNU General Public License is a free, copyleft license for software and other kinds of works.
+
+    The licensed material and derivatives may be used for commercial purposes.
+    The licensed material may be distributed.
+    The licensed material may be modified.
+    This license provides an express grant of patent rights from contributors.
+    The licensed material may be used and modified in private.
+
+    Source code must be made available when the licensed material is distributed.
+    A copy of the license and copyright notice must be included with the licensed material.
+    Modifications must be released under the same license when distributing the licensed material.
+    Changes made to the licensed material must be documented.
+
+    This license includes a limitation and/or no liability.
+    This liceense explicitly states that it does NOT provide any warranty.
+
+    The full license document may be read at <https://choosealicense.com/licenses/gpl-3.0/>
+    The repository may be found at  <https://github.com/hanadrizz/twitterbot>
+    """)
 
 @bot.command()
 @commands.has_any_role(*approved)
